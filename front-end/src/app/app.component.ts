9f7433ea8a3f4f9d7c4d3af935945ec4ecd48872
Farkas_Bence <bence.farkas@wysio.ch>
2023-10-26 17:24:29 +0200

    Működik az "Add Task" funkció.
    (cherry picked from commit e8cd35237da76ec086622ce7e450b567f6969e86)

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+class Task{
+  title = '';
+  comment = '';
+
+  constructor(titl:string, comm:string){
+    this.title = titl;
+    this.comment = comm;
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'to_do';
+  
+  taskArray:Task[] = [];
+  
+  addTask(title:string, comment:string){
+    var temp = new Task(title, comment);
+    this.taskArray.push(temp);
+  }
 }
