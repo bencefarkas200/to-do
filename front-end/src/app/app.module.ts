c840360c7e6dcffd2e3b45bf3a6cc2bf5b8b8cd1
Farkas_Bence <bence.farkas@wysio.ch>
2023-10-30 14:22:38 +0100

    Nem használt dashbord component törlése

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { DashbordComponent } from './components/dashbord/dashbord.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
@@ -9,7 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, DashbordComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
