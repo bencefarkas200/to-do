8226b7a129bfdb27b7a4f54c2886fd9f2079f849
Farkas_Bence <bence.farkas@wysio.ch>
2023-11-29 09:52:51 +0100

    Add TaskSchema to "app.module.ts"

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Task, TaskSchema } from './schemas/task.schema';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:3000')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:3000'),
+    MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
