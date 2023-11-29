e411dd4658f86dfab2b2c77a86bfe7ca967c8c2f
Farkas_Bence <bence.farkas@wysio.ch>
2023-11-29 10:08:03 +0100

    Create service
    Created "task.service.ts" and made "getAll" function

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Task, TaskSchema } from './schemas/task.schema';
+import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:3000'),
     MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }]),
+    TaskModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ back-end/src/task/task.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Task, TaskDocument } from 'src/schemas/task.schema';
+
+@Injectable()
+export class TaskService {
+  constructor(@InjectModel(Task.name) private taskModel: Model<TaskDocument>) {}
+
+  async getAll(): Promise<Task[]> {
+    return await this.taskModel.find().exec();
+  }
+}
