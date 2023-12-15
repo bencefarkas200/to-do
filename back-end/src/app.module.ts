cf4af494ebb7d4883b043416904d41676a268357
Farkas_Bence <bence.farkas@wysio.ch>
2023-12-15 14:17:25 +0100

    Remove unused AppController and AppService files and classes

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Task, TaskSchema } from './schemas/task.schema';
 import { TaskModule } from './task/task.module';
@@ -14,7 +12,7 @@ import { TaskService } from './task/task.service';
     MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }]),
     TaskModule,
   ],
-  controllers: [AppController],
-  providers: [AppService, TaskService],
+  controllers: [],
+  providers: [TaskService],
 })
 export class AppModule {}
