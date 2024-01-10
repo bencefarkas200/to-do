3e46b3dc1ca041c6af7d3eadf0f619f108c8dad4
Farkas_Bence <bence.farkas@wysio.ch>
2024-01-10 13:03:27 +0100

    Match expected return type with actual return type

--- front-end/src/app/task.service.ts
+++ front-end/src/app/task.service.ts
@@ -30,7 +30,7 @@ export class TaskService {
     return this.http.put<Task>(url, '');
   }
 
-  deleteDoneTasks(): Observable<Task> {
-    return this.http.delete<Task>(this.url);
+  deleteDoneTasks(): Observable<void> {
+    return this.http.delete<void>(this.url);
   }
 }
